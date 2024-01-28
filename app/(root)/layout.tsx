3f50d88ae0b43b57a7e677e456fe42320f7c5b00
Manthan Kansara <manthankansara7@gmail.com>
2024-01-28 10:56:04 +0000

    Implemented dashboard setup

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,30 @@
+import prismdb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs"
+import { redirect } from "next/navigation";
+
+export default async function SetupLayout({
+    children
+}: {
+    children: React.ReactNode
+}) {
+    const { userId } = auth();
+
+    if(!userId) {
+        redirect("/sign-in")
+    }
+
+    const store = await prismdb.store.findFirst({
+        where: {
+            userId
+        }
+    });
+
+    if(store) {
+        redirect(`/${store.id}`)
+    }
+    return (
+        <>
+            {children}
+        </>
+    )
+}
